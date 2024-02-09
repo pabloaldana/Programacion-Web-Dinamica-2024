60204c06374f80309f182c14691176f5f9e4f9f1
Pablo Aldana <pabloaldana.cipo@gmail.com>
2024-02-09 19:26:30 -0300

    head tp1 corregidos

--- views/tp1/ej8.php
+++ views/tp1/ej8.php
@@ -1,25 +1,15 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Title</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-        />
+        <?php
+            $title = 'TP1 Ejercicio 6';
+            include_once '../../config/configuracion.php';
+            include_once '../structures/head.php'
+        ?>
     </head>
 
     <body>
-        <?php include_once './structures/header.php';?>
+        <?php include_once '../structures/header.php';?>
         
         <main>
             <div class="container py-5 text-center mt-5 mb-5" style="border:2px solid red; height: 500px">
