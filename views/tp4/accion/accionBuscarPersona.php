26ef453c19d9d4af016b9834e60aaa41ac1ad72e
Pablo Aldana <pabloaldana.cipo@gmail.com>
2024-02-18 23:10:23 -0300

    buscarPersona resuelto

--- views/tp4/accion/accionBuscarPersona.php
+++ views/tp4/accion/accionBuscarPersona.php
@@ -8,13 +8,11 @@
     $objPersona = new AbmPersona();
 
     
-    //echo $patente;
 
     if (!empty($datos)){
         $dni = $datos['dni'];
-        $parametros = array ('DniDuenio'  => $dni);
-        $personaBuscada = $objPersona -> buscar($parametros); //aca se rompe
-        
+        $parametros = array ('dni'  => $dni);
+        $personaBuscada = $objPersona -> buscar($parametros);       
         
     }
 ?>
@@ -31,29 +29,7 @@
     <?php include_once '../../structures/header.php';?>
 
     <div class="container">
-        <div>
-            <table class="table caption-top">
-                <caption>Datos de la Persona <?php ?> </caption>
-                <thead>
-                    <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Apellidp</th>
-                        <th scope="col">Dni</th>
-                        <th scope="col">Fecha de Nacimiento</th>
-                        <th scope="col">Telefono</th>
-                        <th scope="col">Direccion</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row"><?php ?></th>
-                        <td><?php ?></td>
-                        <td><?php ?></td>
-                        <td><?php ?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        
     </div>
 
     <?php include_once '../../structures/footer.php'; ?>
--- views/tp4/buscarPersona.php
+++ views/tp4/buscarPersona.php
@@ -10,9 +10,21 @@
 
     <?php include_once '../structures/header.php';?>
 
-    <body>
+    <div class="container d-flex mt-3">
+        <div class="card mb-3" style="margin:auto;">
+            <div class="card-header">Buscar persona</div>
+            <div class="card-body">
+                <p class="card-text">Ingrese el dni de la persona que esta buscando</p>
+                <form action="./accion/accionBuscarPersona.php" method="post" id="form" name="form" class="form" novalidate>
+                    <input type="text" class="form-control" id="dni" name="dni" placeholder="DNI" required>
+                    <div class="invalid-feedback mb-3">Ingresar el numero de dni</div>
+                    <button class="btn btn-primary" type="submit">Buscar</button>    
+                </form>
+ 
+            </div>
+        </div>
+    </div>
 
-    </body>
-
+    <script src="./js/script.js"></script>
     <?php include_once '../structures/footer.php';?>
 </html>
